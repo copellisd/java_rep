803a3270e7c1d2cb61fbb474a801cc9224a58c84
copellisd <156243215+copellisd@users.noreply.github.com>
2024-01-12 14:49:25 -0600

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,12 @@
+class hello
+{
+    public static void main(String a[])
+    {
+        int num1=3;
+       // int result=++num1;
+        //int num2=5;
+        //int result=num1+num2;
+        num1+=2;
+        System.out.println(num1);
+    }
+}
